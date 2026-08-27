106e68c6c743d8fd5db106a10c6ced5370d29b81
Niti157 <niti.pak@rmutto.ac.th>
2026-08-27 12:12:54 +0700

    week07 commit

---
+++ app/week07/[id]/page.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { shops } from "../components/shopItem";
+import Loading from "../components/Loading";
+import { Suspense } from "react";
+
+
+export default async function ShopDetail({params}) {
+    const {id} = await params;
+    const shop = shops.find(
+        item => item.id === Number(id)
+    );
+    const Status = (sta: boolean) => {
+        if (sta)
+            return <span style={{ color: "green" }}>open</span>;
+
+        return <span style={{ color: "red" }}>close</span>;
+    };
+    return(
+        <>
+        <Suspense fallback={<Loading/>}>
+        <div className="w-xl mx-auto p-6">
+      <h1 className="text-3xl font-bold">
+        Shop Detail
+      </h1>
+
+      <div
+        key={shop.id}
+        className="border rounded-lg p-4 m-4"
+      >
+        <p className="mt-4 font-semibold">
+          ID:{shop.id}
+        </p>
+        <p className="my-4">
+          Title:{shop.title}
+        </p>
+        <p className="my-4">
+          Open Status:{Status(shop.openStatus)}
+        </p>
+      </div>
+
+      <Link
+        href="/week07/"
+        className="bg-gray-600 text-white px-4 py-2 rounded"
+      >Back</Link>
+
+    </div>
+        </Suspense>
+        </>
+    );
+}
---
+++ app/week07/components/shopItem.tsx
@@ -0,0 +1,14 @@
+export const shops =[
+    {
+        id: 1,
+        title: "Adidas",
+        location: {lat:100,long:100},
+        openStatus: false
+    },
+    {
+        id: 2,
+        title: "apple store",
+        location :{lat:105,long:109},
+        openStatus: true
+    }
+];
